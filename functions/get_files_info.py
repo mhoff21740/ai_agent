c7a1b16a9f57dddfa317dfb2a94bdf363e66173b
mhoff21740 <marshall.hoffmaster@gmail.com>
2025-07-07 19:08:10 -0400

    Finished CH2:L2, start on CH2:L3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,9 +1,47 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    full_path = os.path.join(working_directory, directory)
-    full_path = os.path.abspath(full_path)
-    if not full_path.startswith(working_directory):
-        f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
-        f'Error: "{directory}" is not a directory'
+    combined_path = os.path.join(working_directory, directory)
+    combined_path = os.path.abspath(combined_path)
+    cleaned_working = os.path.abspath(working_directory)
+    if not combined_path.startswith(cleaned_working):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(combined_path):
+        return f'Error: "{directory}" is not a directory'
+    info_list = []
+    try:
+        for item in os.listdir(combined_path):
+            full_path = os.path.join(combined_path, item)
+            filename = os.path.basename(full_path)
+            file_size = os.path.getsize(full_path)
+            if os.path.isdir(full_path):
+                result = True 
+            else:
+                result = False
+            file_info = f"- {filename}: file_size={file_size} bytes, is_dir={result}\n"
+            info_list.append(file_info)
+        return "\n".join(info_list)
+    except Exception as e:
+        return f"Error: {e}"
+
+  
+                    
+       
+    
+        
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
